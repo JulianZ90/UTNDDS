951119fb9f0870d409d8abf44cf5cab878b6ba9e
fdodino <fdodino@e1a88654-4610-4beb-b2e6-8ed2f74c6698>
2013-04-16 11:43:41 +0000

    Versión inicial: los traits son independientes del cliente. Esto trae muchas desventajas para testear los componentes.

---
+++ examples/dds/objetos-patrones/clientesTarjetaCredito/clientesTarjetaCredito-domain-scala/trunk/src/ar/edu/clientes/Promocion.scala
@@ -0,0 +1,19 @@
+package ar.edu.clientes
+
+import ar.edu.clientes.exceptions.BusinessException
+
+trait Promocion {
+  var puntosAcumulados = 0
+
+  def comprar(monto: Int) =
+    if (monto > montoMinimo) {
+      puntosAcumulados += puntosPromocion
+      print ("Puntos acumulados: " + puntosAcumulados)
+    }
+
+  def montoMinimo(): Int = 50
+  
+  def puntosPromocion(): Int = 15
+  
+}
+
