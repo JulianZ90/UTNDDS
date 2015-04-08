0392b4617c792fa67bca939c07e82d011130747d
fdodino <fdodino@e1a88654-4610-4beb-b2e6-8ed2f74c6698>
2015-04-08 13:58:25 +0000

    Eliminamos proyecto deprecado

--- examples/dds/objetos-patrones/listaCorreo/suscripcionAbiertaYCerrada-envioRestringidoYLibre  (@Deprecated)/feoDecorator/src/ar/org/uqbar/disenio/listas/decorator/EnvioValidator.xtend
+++
@@ -1,5 +0,0 @@
-package ar.org.uqbar.disenio.listas.decorator
-
-interface EnvioValidator {
-	def void validate(Post post, ListaCorreoImpl lista)
-}
--- examples/dds/objetos-patrones/listaCorreo/suscripcionAbiertaYCerrada-envioRestringidoYLibre  (@Deprecated)/mixin-trait-herenciaMultiple/src/org/uqbar/disenio/scala/listas/EmailSenderProvider.scala
+++
@@ -1,7 +0,0 @@
-package org.uqbar.disenio.scala.listas
-import org.uqbar.disenio.scala.emails.EmailSender
-
-object EmailSenderProvider  {
-	var emailSender:EmailSender = null
-}
-
