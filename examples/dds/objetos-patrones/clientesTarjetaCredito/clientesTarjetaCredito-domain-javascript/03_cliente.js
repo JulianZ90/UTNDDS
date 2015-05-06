6e09d83394e14c75d2d4199c95e1881737d557ad
fdodino <fdodino@e1a88654-4610-4beb-b2e6-8ed2f74c6698>
2015-05-06 20:01:51 +0000

    Version inicial

---
+++ examples/dds/objetos-patrones/clientesTarjetaCredito/clientesTarjetaCredito-domain-javascript/03_cliente.js
@@ -0,0 +1,58 @@
+function clone(object) {
+    function F() {}
+    F.prototype = object;
+    return new F();
+}
+
+function Cliente() {
+    "use strict";
+
+    this.deuda = 0;
+    this.puntos = 0;
+}
+
+Cliente.prototype = {
+    pagarVencimiento: function(monto) {
+        "use strict";
+        this.deuda = this.deuda - monto;
+    },
+    agregarPuntos: function(unosPuntos) {
+        "use strict";
+        this.puntos = this.puntos + unosPuntos;
+    },
+    getDeuda: function() {
+        return this.deuda;
+    },
+    comprar: function(monto) {
+        this.deuda = this.deuda + monto;
+    },
+    safeShop: function(unMontoMaximo) {
+        var clienteDecorado = clone(this);
+        clienteDecorado.montoMaximo = unMontoMaximo;
+        clienteDecorado.cliente = this;
+        clienteDecorado.comprar = function(monto) {
+            "use strict";
+            if (monto > this.montoMaximo) {
+                throw new Error("No debe comprar por mas de " + this.montoMaximo);
+            }
+            this.cliente.comprar(monto);
+        };
+        return clienteDecorado;
+    },
+    promocion: function() {
+        var clienteDecorado = clone(this);
+        clienteDecorado.cliente = this;
+        clienteDecorado.comprar = function(monto) {
+            "use strict";
+            this.cliente.comprar(monto);
+            if (monto > 50) {
+                this.cliente.agregarPuntos(15);
+            }
+        };
+        return clienteDecorado;
+    }
+};
+
+module.exports = {
+    Cliente: Cliente,
+};
