92b0d8e7daeb3e77a7d2dba3a9e978d84d5d1097
gastonprieto <gastonprieto@e1a88654-4610-4beb-b2e6-8ed2f74c6698>
2015-05-08 14:42:54 +0000

    Separo los ejemplos de clienteTarjetaCredito-domain-js en carpetas y agrego el runner de test para npm

--- objetos-patrones/clientesTarjetaCredito/clientesTarjetaCredito-domain-javascript/test/decorator-prototypical-2/cliente-decorator-prototypical-2.test.js
+++ objetos-patrones/clientesTarjetaCredito/clientesTarjetaCredito-domain-javascript/test/decorator-prototypical-2/cliente-decorator-prototypical-2.test.js
@@ -1,16 +1,14 @@
 var assert = require("assert");
-var Cliente = require("./02_cliente").Cliente;
+var Cliente = require("../../src/decorator-prototypical-2/cliente").Cliente;
 
 var manuel;
 var ricardo;
 var clienteSafe;
 var clientePromo;
 
-describe('clientes', function() {
+describe('Cliente - Decorator Prototypical 2', function() {
     "use strict";
     var clienteSafe;
-    var frank;
-    var clienteSafeYPromo;
 
     beforeEach(function() {
         manuel = new Cliente();
@@ -18,32 +16,37 @@ describe('clientes', function() {
         manuel.deuda = 400;
         clienteSafe = manuel.safeShop(200);
         clientePromo = ricardo.promocion();
-        frank = new Cliente();
-        clienteSafeYPromo = frank.promocion().safeShop(200);
     });
+
     it('should allow to buy', function() {
         clienteSafe.comprar(150);
         assert.equal(550, manuel.getDeuda());
-        // ojo, el clienteSafe publica una deuda vacia
-        assert.equal(0, clienteSafe.getDeuda());
+        // ojo, el clienteSafe no tiene deuda
+        // pero yo veo la deuda del objeto decorado
+        assert.equal(550, clienteSafe.getDeuda());
     });
+
     it('should not allow to buy', function() {
         assert.throws(function() {
             clienteSafe.comprar(250);
         }, Error, "No se puede comprar por mas del monto de safe shop");
     });
+
     it('should not add points', function() {
+        var frank = new Cliente();
+        var clienteSafeYPromo = frank.promocion().safeShop(200);
         assert.throws(function() {
             clienteSafe.comprar(250);
         }, Error, "No se puede comprar por mas del monto de safe shop");
         assert.equal(0, frank.puntos);
     });
+
     it('should add points', function() {
         clientePromo.comprar(150);
+        // si el método promocion() hiciera this.agregarPuntos(...)
+        // ricardo no tendría puntos!!!
+        // pero el cliente promo sí
+        // assert.equal(15, clientePromo.puntos);
         assert.equal(15, ricardo.puntos);
-    }),
-    it('should add points and validate safe shop', function() {
-        clienteSafeYPromo.comprar(150);
-        assert.equal(15, frank.puntos);
     });
 });
